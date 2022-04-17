cd4603a84486d8791100fadddb0d0d63fd2722b4
Muhammad Furqan <furqan@tokopedia.com>
2022-04-17 13:27:40 +0700

    Maps

--- app/src/main/java/com/furqan/ilt_5_intermediate_android/MainActivity.kt
+++ app/src/main/java/com/furqan/ilt_5_intermediate_android/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.furqan.ilt_5_intermediate_android
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.furqan.ilt_5_intermediate_android.databinding.ActivityMainBinding
+import com.furqan.ilt_5_intermediate_android.maps.MapsActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding) {
+            btnMaps.setOnClickListener {
+                startActivity(Intent(this@MainActivity, MapsActivity::class.java))
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/furqan/ilt_5_intermediate_android/maps/MapsActivity.kt
@@ -0,0 +1,12 @@
+package com.furqan.ilt_5_intermediate_android.maps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.furqan.ilt_5_intermediate_android.R
+
+class MapsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+    }
+}
